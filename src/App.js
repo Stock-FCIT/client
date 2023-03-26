4878926f655d193991fe5a1b1bcfaa0e3e86db5b
Yurko040503 <yurkobyrko@gmail.com>
2023-03-27 00:13:35 +0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Header />
+        <Routes>  
+          {/*
+          <Route path="/" element={<div>VAsyl dyakuiu</div>}/>
+          <Route path="/about" element={<div>Vasyl programmer</div>}/>
+           */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Header.scss";
+import logo from "../../images/stock_logo.svg";
+import liked from "../../images/liked.svg";
+import cart from "../../images/cart.svg";
+
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <div className="header-left">
+        <img className="logo" src={logo} alt="Logo" />
+        <h1 className="logo-name">Stock</h1>
+      </div>
+
+      <div className="header-right">
+        <a className="svg-button" href="">
+          <img src={liked} alt="" />
+        </a>
+        <a className="svg-button" href="">
+          <img src={cart} alt="" />
+        </a>
+        <a className="ebujaknazvatu" href="">Register</a>
+        <p className="separator">|</p>
+        <a className="ebujaknazvatu" href="">Log In</a>
+       </div>
+    </div>
+  );
+};
+
+export default Header;
