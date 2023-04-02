1e09c00117d74232b3faa90cd9c0ca30a32996eb
Vadym Kobylan <vadym.kobylan@gmail.com>
2023-04-02 22:58:55 +0300

    Added item form

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import './Main.scss';
 
 import SortBar from '../../components/SortBar/SortBar.jsx';
+import Item from '../../components/Item/Item';
 import leftPalm from '../../images/leftPalm.png';
 import rightPalm from '../../images/rightPalm.png';
 
@@ -11,14 +12,20 @@ const Main = () => {
     <>
       <img src={leftPalm} alt="" className="leftPalm" />
       <img src={rightPalm} alt="" className="rightPalm" />
-      <SortBar />
-      <div className="wrapper">
-        {/* <img className='leftPalm' src={leftPalm} alt="leftPalm" />
-        <img className='rightPalm' src={rightPalm} alt="rightPalm" /> */}
-        <div className="item-holder">
-          <img src="" alt="" />
+      <div className="mainWrapper">
+        <SortBar />
+        <div className="wrapper">
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+          <Item />
         </div>
       </div>
+      apper
     </>
   );
 };
