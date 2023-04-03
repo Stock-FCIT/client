3737ec8e4e3424634ea54b43ca4d852b087518ac
Vadym Kobylan <vadym.kobylan@gmail.com>
2023-04-03 19:55:47 +0300

    Linked Plant List from Back-End

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Main.scss';
 
@@ -6,30 +6,27 @@ import SortBar from '../../components/SortBar/SortBar.jsx';
 import Item from '../../components/Item/Item';
 import leftPalm from '../../images/leftPalm.png';
 import rightPalm from '../../images/rightPalm.png';
+import { useEffect } from 'react';
+import { fetchPlants } from '../../http/plantsAPI';
 
 const Main = () => {
+  const [plants, setPlants] = useState();
+
+  useEffect(() => {
+    fetchPlants().then((data) => setPlants(data.rows));
+  }, []);
+
+  console.log(plants);
+
   return (
     <>
       <img src={leftPalm} alt="" className="leftPalm" />
       <img src={rightPalm} alt="" className="rightPalm" />
       <div className="mainWrapper">
         <SortBar />
-        <div className="wrapper">
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-        </div>
+        <div className="wrapper">{plants ? plants.map((obj) => <Item key={obj.id}  {...obj}/>) : <></>}</div>
       </div>
-      apper
+      
     </>
   );
 };
--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -2,20 +2,19 @@ import React from 'react';
 
 import './Item.scss';
 
-import itemImage from '../../images/plant.jpg';
 import { ReactComponent as Favourite } from '../../images/liked.svg';
 
-const Item = () => {
+const Item = ({ img, name, price }) => {
   return (
     <div className="itemHolder">
-      <img className="itemImage" src={itemImage} alt="" />
+      <img className="itemImage" src={process.env.REACT_APP_API_URL + img} alt="" />
 
       <div className="itemInfo">
         <div className="favouriteWrapper">
           <Favourite className="favouriteButton" />
         </div>
-        <div className="itemName">Red Prayer Plant</div>
-        <div className="itemPrice">$79.89</div>
+        <div className="itemName">{name}</div>
+        <div className="itemPrice">${price}</div>
       </div>
     </div>
   );
