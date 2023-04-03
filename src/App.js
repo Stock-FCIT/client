0d6a70377cc2abc94600da292caf9759d8424c95
Yurko040503 <yurkobyrko@gmail.com>
2023-04-03 21:22:19 +0300

    Add Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import Main from "./Pages/Main/Main";
 
 function App() {
@@ -8,11 +9,10 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Header />
-        <Routes>  
-          {
-          <Route path="/" element={<Main/>}/>
-          }
-        </Routes>
+        <div className="body">
+          <Routes>{<Route path="/" element={<Main />} />}</Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => {
+    return (
+        <div className='footerContainer'>
+            <a className='privacy'>Copyright © 2023. Privacy Policy.</a>
+        </div>
+    );
+};
+
+export default Footer;
