5ae80a1ba2e2fd64660495e0b29fe2f23f498945
Yurko040503 <yurkobyrko@gmail.com>
2023-04-04 18:53:29 +0300

    User page and initial menu

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.scss";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Main from "./Pages/Main/Main";
+import UserPage from "./Pages/UserPage/UserPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Header />
         <div className="body">
           <Routes>{<Route path="/" element={<Main />} />}</Routes>
+          <Routes>{<Route path="/userPage" element={<UserPage />} />}</Routes>
         </div>
         <Footer />
       </BrowserRouter>
