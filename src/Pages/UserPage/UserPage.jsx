46c2539203ca01b8df74594178d6ce4ebd2b470d
Vadym Kobylan <vadym.kobylan@gmail.com>
2023-04-09 15:57:22 +0300

    Autofill with available information

--- src/Pages/UserPage/UserPage.jsx
+++ src/Pages/UserPage/UserPage.jsx
@@ -1,47 +1,48 @@
-import React, { useState } from "react";
-import userPhoto from "../../images/userPhoto.png";
-import styles from "./UserPage.module.scss";
-import EditInfo from "../../components/EditInfo/EditInfo";
-import EditPassword from "../../components/EditPassword/EditPassword";
+import React, { useState, useEffect } from 'react';
+import userPhoto from '../../images/userPhoto.png';
+import styles from './UserPage.module.scss';
+import EditInfo from '../../components/EditInfo/EditInfo';
+import EditPassword from '../../components/EditPassword/EditPassword';
+
+import { getUserInfo } from '../../http/userAPI';
 
 const UserPage = () => {
   const [menuHandler, setMenuHandler] = useState(1);
+  const [userInfo, setUserInfo] = useState({});
+
+  useEffect(() => {
+    getUserInfo().then((data) => {
+      setUserInfo(data);
+    });
+  }, []);
+
   return (
     <div className={styles.userContainer}>
       <img className={styles.userPhoto} src={userPhoto} alt="userPhoto" />
-      <div className={styles.userName}>Tony Stark</div>
+      <div className={styles.userName}>{userInfo.name}</div>
 
       <div className={styles.menu}>
         <div className={styles.menuButtonContainer}>
           <div
-            className={
-              menuHandler === 1 ? styles.menuButtonActive : styles.menuButton
-            }
-            onClick={() => setMenuHandler(1)}
-          >
+            className={menuHandler === 1 ? styles.menuButtonActive : styles.menuButton}
+            onClick={() => setMenuHandler(1)}>
             Edit Account
           </div>
           <div
-            className={
-              menuHandler === 2 ? styles.menuButtonActive : styles.menuButton
-            }
-            onClick={() => setMenuHandler(2)}
-          >
+            className={menuHandler === 2 ? styles.menuButtonActive : styles.menuButton}
+            onClick={() => setMenuHandler(2)}>
             Orders History
           </div>
           <div
-            className={
-              menuHandler === 3 ? styles.menuButtonActive : styles.menuButton
-            }
-            onClick={() => setMenuHandler(3)}
-          >
+            className={menuHandler === 3 ? styles.menuButtonActive : styles.menuButton}
+            onClick={() => setMenuHandler(3)}>
             Favourites
           </div>
         </div>
 
         {menuHandler === 1 ? (
           <div>
-            <EditInfo />
+            <EditInfo user={userInfo} />
             <EditPassword />
           </div>
         ) : menuHandler === 2 ? (
