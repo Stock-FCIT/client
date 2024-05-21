cb29533508a30a8405aa359e4be4e2f4966ca74d
Yurko040503 <yurkobyrko@gmail.com>
2024-05-21 11:21:27 +0300

    lalalla

--- src/index.js
+++ src/index.js
@@ -2,14 +2,8 @@ import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import UserStore from './store/UserStore';
-import ReactGA from 'react-ga4';
 
 export const Context = createContext(null);
-
-ReactGA.initialize('G-XEKTH13474', {
-  debug: true,
-  titleCase: false,
-});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
